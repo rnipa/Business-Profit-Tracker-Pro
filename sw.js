5cf55ed6b0768874a4b5081403d079f2dbc7a236
rnipa <74035707+rnipa@users.noreply.github.com>
2026-09-05 00:00:34 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='bpt-pro-pwa-v3';
+const ASSETS=['./','./index.html','./manifest.json','./logo.svg','./favicon.png','./icons/icon-192.png','./icons/icon-512.png','./icons/icon-192-maskable.png','./icons/icon-512-maskable.png','./screenshots/dashboard.png','./screenshots/mobile.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const cp=r.clone();caches.open(CACHE).then(x=>x.put(e.request,cp));return r}).catch(()=>caches.match('./index.html'))))});
