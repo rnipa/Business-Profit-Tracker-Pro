d0e57d1a965508eafa6f3432a4448ca85d73b38b
rnipa <74035707+rnipa@users.noreply.github.com>
2026-09-05 01:26:08 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE='bpt-pro-pwa-final-v4';
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.svg',
+  './logo-1024.png',
+  './favicon.png',
+  './favicon.ico',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-192-maskable.png',
+  './icon-512-maskable.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => {
+      if (cached) return cached;
+      return fetch(event.request).then(response => {
+        if (response && response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
+});
